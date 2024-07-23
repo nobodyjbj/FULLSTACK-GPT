6146d925a51fa4ea2f9c9b4b37418ea81ced839a
nobodyjbj <nobody.jbj@gmail.com>
2024-07-23 12:57:36 +0900

    ..

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,27 +1,56 @@
+from langchain.embeddings import CacheBackedEmbeddings
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+
+# from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import UnstructuredFileLoader
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores.faiss import FAISS
 import streamlit as st
-import time
+
 
 st.set_page_config(
     page_title="DocumentGPT",
-    page_icon="📑",
+    page_icon="📃",
 )
+
+
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader("./files/chapter_one.txt")
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
 
 st.title("DocumentGPT")
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+st.markdown(
+    """
+Welcome!
+            
+Use this chatbot to ask questions to an AI about your files!
+"""
+)
 
+file = st.file_uploader(
+    "Upload a .txt .pdf or .docx file",
+    type=["pdf", "txt", "docx"],
+)
 
-def send_message(message, role):
-    with st.chat_message(role):
-        st.write(message)
-        messages.append({"message": message, "role": role})
-    st.write(messages)
-
-
-message = st.chat_input("Send a message to the ai.")
-
-if message:
-    send_message(message, "human")
-    time.sleep(2)
-    send_message(f"You said: {message}", "ai")
+if file:
+    retriever = embed_file(file)
+    s = retriever.invoke("winston")
+    s
